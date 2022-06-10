01311d941024a7e76c34b27b13d8e671031da9f3
Thiago Alves <thiagoacsp@gmail.com>
2022-06-09 21:44:56 -0300

    Recuperação de senha

--- index.php
+++ index.php
@@ -149,7 +149,72 @@ $app->post("/admin/users/:iduser", function($iduser){
 
 });
 
+$app->get("/admin/forgot", function(){
 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = USER::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});
+
+$app->get("/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecreypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+});
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecreypt($_GET["code"]);
+
+	USER::setForgotUser($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
 
 $app->run();
 
