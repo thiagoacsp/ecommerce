86a20183377b40f1b14fb6723e33c0780ea11661
Thiago Alves <thiagoacsp@gmail.com>
2022-06-11 15:39:47 -0300

    Site - Categorias

--- index.php
+++ index.php
@@ -300,6 +300,20 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'productis'=>[]
+	]);
+});
+
 $app->run();
 
  ?>
