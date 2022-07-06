1c9cf3a3963478909ecb4e14bc51d7e4c5af1b54
Thiago Alves <thiagoacsp@gmail.com>
2022-07-06 19:40:38 -0300

    Detelhes do produto

--- site.php
+++ site.php
@@ -43,4 +43,18 @@ $app->get("/categories/:idcategory", function($idcategory){
 		'pages'=>$pages
 	]);
 });
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+});
 ?>
